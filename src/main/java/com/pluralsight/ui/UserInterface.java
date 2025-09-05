21069fb586df3fb7876005c2feb0d099649103b4
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-09-05 12:16:20 -0700

    Update UserInterface.java

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -17,8 +17,24 @@ public class UserInterface {
             Enemy enemy = new Enemy("Goblin", 50, 5, 20);
 
             while(player.getHp() != 0 && enemy.getHp() != 0){
-                player.attack(enemy);
-                System.out.println("enemy current hp is now" + enemy.getHp());
+                System.out.println("You have encountered a " + enemy.getName() +"\n" +
+                        "Choose what you would like to do. \n" +
+                        "1. Fight! \n" +
+                        "2. Escape! ");
+                int player_encounter_choice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (player_encounter_choice == 1){
+                    //attack,skills,magic,items
+                } else if (player_encounter_choice == 2) {
+                    //random chance to escape if successful exit combat end game else force combat and be attacked first
+                }
+
+
+//                player.attack(enemy);
+//                System.out.println("enemy current hp is now" + enemy.getHp());
+
+
             }
 
 
